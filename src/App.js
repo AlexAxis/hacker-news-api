47c9a0e4c168c480016905653d86b5d5f7f43121
Alexis Sousa <alexis-sousa@hotmail.com>
2021-08-12 09:22:28 +0100

    ✨  Create API requests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import HackerCard from './components/HackerCard'
+import Pagination from './components/Pagination'
 function App() {
+  const [topList, setTopList] = useState([])
+  const [resultList, setResultList] = useState({})
+  const [numPages, setNumPages] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  // * Fetch -> GET top hacker list
+  useEffect(() => {
+    const getList = () =>
+      fetch(
+        `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          setTopList(json)
+          setNumPages(json.length)
+        })
+        .catch((err) => {
+          throw err
+        })
+    const interval = setInterval(() => {
+      getList()
+    }, 300000)
+    if (topList.length === 0) {
+      return getList()
+    }
+    return () => clearInterval(interval)
+  }, [])
+
+  // * Fetch -> GET single result from the top list
+  useEffect(async () => {
+    if (topList.length > 0) {
+      let auxList = {}
+      for (var i = currentPage * 10 - 10; i < currentPage * 10; i++) {
+        await fetch(
+          `https://hacker-news.firebaseio.com/v0/item/${topList[i]}.json?print=pretty`
+        )
+          .then((response) => response.json())
+          .then((json) => {
+            auxList[topList[i]] = json
+          })
+          .catch((err) => {
+            throw err
+          })
+        await setResultList({ ...auxList })
+      }
+    }
+  }, [topList, currentPage])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="WrapCards">
+        {Object.keys(resultList).length > 0 &&
+          Object.keys(resultList).map((key) => {
+            return <HackerCard key={key} resultList={resultList[key]} />
+          })}
+      </div>
+
+      <Pagination
+        currentPage={currentPage}
+        numPages={numPages / 10}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
